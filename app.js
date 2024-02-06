1427ba9e759a279f1c776d24ff9ba48c79fa3100
adam <a.d.a.m.io@icloud.com>
2024-02-06 17:30:39 +0000

    new design and layout

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
+const cardsContainer = document.querySelector('.app__cards')
+const modal = document.querySelector('.app__modal')
+const modalCloseButton = document.querySelector('.app__modal-close')
+const modalName = document.querySelector('.app__modal-name')
+const modalNumber = document.querySelector('.app__modal-number')
+const modalImage = document.querySelector('.app__modal-image')
+const modalDescr = document.querySelector('.app__modal-descr')
 
-// let appData = [];
-const app = document.querySelector('.app')
+modalCloseButton.addEventListener('click', function() {
+  closeModal();
+})
+
+function openModal(name, number, image, descr, type) {
+  modalName.innerHTML = name
+  modalNumber.innerHTML = number
+  modalDescr.innerHTML = descr
+
+  modalImage.src = image
+  modalImage.classList.add(type)
+
+  modal.classList.toggle('active')
+}
+
+function closeModal() {
+  modal.classList.toggle('active')
+
+  modalImage.classList = "app__modal-image";
+}
 
 fetch('./assets/data/pals.json', {
     headers : { 
@@ -12,39 +37,33 @@ fetch('./assets/data/pals.json', {
 .then((data) => {
      console.log(data);
 
-    data.forEach(element => {
+    data.forEach(pal => {
         let newCard = document.createElement('div')
         newCard.classList.add('card')
+        newCard.classList.add(pal.types[0] + "-type")
 
         let newImage = document.createElement('img')
-        newImage.src = "https://raw.githubusercontent.com/mlg404/palworld-paldex-api/main" + element.image
+        newImage.src = "https://raw.githubusercontent.com/mlg404/palworld-paldex-api/main" + pal.image
         newImage.classList.add('card__image')
-        newImage.classList.add(element.types[0])
+        newImage.classList.add(pal.types[0])
 
-        let newTitle = document.createElement('h1')
+        let newTitle = document.createElement('h4')
         newTitle.classList.add('card__title')
-        newTitle.innerHTML = element.name
-        newTitle.setAttribute('data-value', element.name)
-        
-        let newType = document.createElement('p')
-        newType.classList.add('card__type-text')
-        newType.classList.add(element.types[0])
-        newType.innerHTML = element.types[0]
-        let typeContainer = document.createElement('div')
-        typeContainer.classList.add('card__type')
-        typeContainer.appendChild(newType)
+        newTitle.innerHTML = pal.name
+        newTitle.setAttribute('data-value', pal.name)
 
-        let newDescr = document.createElement('p')
-        newDescr.classList.add('card__descr')
-        newDescr.innerHTML = element.description
-        
+        let newNumber = document.createElement('p')
+        newNumber.classList.add('card__number')
+        newNumber.innerHTML = pal.key
 
         newCard.appendChild(newImage)
         newCard.appendChild(newTitle)
-        newCard.appendChild(typeContainer)
-        newCard.appendChild(newDescr)
-        app.appendChild(newCard)
+        newCard.appendChild(newNumber)
 
-        console.log(element.name)
+        newCard.addEventListener('click', function() {
+          openModal(pal.name, pal.key, newImage.src, pal.description, pal.types[0])
+        });
+
+        cardsContainer.appendChild(newCard)
     });
 });
